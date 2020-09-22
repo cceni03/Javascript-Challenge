8023971a7205b9df8b5348c9c427d48560ad51fd
cceni03 <czc5230.cc@gmail.com>
2020-09-21 23:13:11 -0400

    Update

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -10,16 +10,20 @@ var filterState = 'Pennsylvania';
 // Reference Table Body
 var tbody = d3.select("tbody");
 
-// Display UFO Sighting
-tableData.foreach(function(ufoSighting) {
-    console.log(ufoSighting);
-    var row = tbody.append("tr");
-    Object.entries(ufoSighting).forEach(function([key, value]) {
-        console.log(key, value);
-        var cell = row.append("td");
-        cell.text(value);
+// Load Data
+loadTable(tableData);
+
+function loadTable(table){
+    table.forEach((ufoSightings) => {
+        var row = tBody.append ("tr");
+        Object.entries(ufoSightings).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
     });
-});
+}
+
+
 
 // Select Button
 var button = d3.select("filter-btn");
