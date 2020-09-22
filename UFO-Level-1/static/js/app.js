544d7f66f4fa7665f3796bb081743b30e8af0db9
cceni03 <czc5230.cc@gmail.com>
2020-09-21 21:45:32 -0400

    Update

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -5,3 +5,8 @@ console.log(tableData);
 // Reference Table Body
 var tbody = d3.select("tbody");
 
+// UFO Sighting
+tableData.foreach(function(ufoSighting) {
+    console.log(ufoSighting);
+    var row = tbody.append("tr");
+})
